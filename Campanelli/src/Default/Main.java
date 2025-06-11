d37612806f021f3f46a5f2127e6a24163772c2a3
amirandabdev <angelo.miranda.bdev@gmail.com>
2025-06-11 09:14:20 +0200

    Aggiornato dichiarazione Campanelli

--- Campanelli/src/Default/Main.java
+++ Campanelli/src/Default/Main.java
@@ -3,7 +3,7 @@ package Default;
 public class Main {
 
 	public static void main(String[] args) {
-		Campanello[] Campanelli = new Campanello[2];
+		Campanello[] Campanelli = new Campanelli[2];
 
 		Campanelli[0] = new Campanello("c1", "din");
 		Campanelli[1] = new Campanello("c2", "dan");
