8c6db136bc4bbbfc8da21a850bea1f2e19a3c705
lyone <lyone@MD>
2025-06-11 09:13:51 +0200

    aggiornato main

--- Campanelli/src/Default/Main.java
+++ Campanelli/src/Default/Main.java
@@ -3,12 +3,12 @@ package Default;
 public class Main {
 
 	public static void main(String[] args) {
-		Campanello[] Campanelli = new Campanello[2];
+		Campanelli[] Campanelli = new Campanelli[2];
 
-		Campanelli[0] = new Campanello("c1", "din");
-		Campanelli[1] = new Campanello("c2", "dan");
+		Campanelli[0] = new Campanelli("c1", "din");
+		Campanelli[1] = new Campanelli("c2", "dan");
 
-		for (Campanello Campanello : Campanelli)
+		for (Campanelli Campanello : Campanelli)
 			System.out.println(Campanello + "," + Campanello.suona());
 
 	}
