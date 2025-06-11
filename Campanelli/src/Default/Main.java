e9797ee5a2a354d7a71146d2b9e1557af019beee
lyone <lyone@MD>
2025-06-11 09:12:18 +0200

    aggiunto main

--- Campanelli/src/Default/Main.java
+++ Campanelli/src/Default/Main.java
@@ -3,7 +3,13 @@ package Default;
 public class Main {
 
 	public static void main(String[] args) {
-	
+		Campanello[] Campanelli = new Campanello[2];
+
+		Campanelli[0] = new Campanello("c1", "din");
+		Campanelli[1] = new Campanello("c2", "dan");
+
+		for (Campanello Campanello : Campanelli)
+			System.out.println(Campanello + "," + Campanello.suona());
 
 	}
 
