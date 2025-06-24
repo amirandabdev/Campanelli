f02b1f860af55d58e63e9a3f11afeb5687994791
lyone <lyone@MD>
2025-06-24 12:22:50 +0200

    factory

--- Campanelli/src/Default/DoorBellFactory.java
+++ Campanelli/src/Default/DoorBellFactory.java
@@ -4,7 +4,7 @@ public class DoorBellFactory implements BellFactorySdk{
 
 	@Override
 	public BellSdk createBell() {
-		// TODO Auto-generated method stub
+		
 		return new DoorBell("Via Giovanni 27", "din");
 	}
 
